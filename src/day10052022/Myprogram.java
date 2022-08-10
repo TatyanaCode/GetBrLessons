80313feef334765a35e93bb715e3db2fa82394d7
danilova.tatyana <danilov.fedor@otr.ru>
2022-08-10 15:22:49 +0300

    day10082022

---
+++ src/day10052022/Myprogram.java
@@ -0,0 +1,19 @@
+package day10052022;
+
+import java.util.Scanner;
+
+/**
+ * @author tatyana.danilova 10.08.2022 14:34
+ * Вводятся два целых числа a и b. Выведите на экран все числа от a до b включительно.
+ */
+public class Myprogram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (b>0)
+            while(a<=b){ System.out.println(a); a++;}
+        else
+            while (a>=b){System.out.println(a);a--;}
+    }
+}
