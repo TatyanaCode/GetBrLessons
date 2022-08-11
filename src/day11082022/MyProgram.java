56222ac06e1f21d2189e6f057c7f701d9a2c0bf0
danilova.tatyana <danilov.fedor@otr.ru>
2022-08-11 22:18:02 +0300

    day11082022

--- src/day11082022/MyProgram.java
+++ src/day11082022/MyProgram.java
@@ -11,7 +11,25 @@ import java.util.Scanner;
 public class MyProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        {
+            int k = 0;
+            if (n % x == 0){
+                k = n;
+            } else {
+                int l = n/x;
+                k = l * x + x;
+            }
+            System.out.println(k);
+        }
+        {
+            int k = 0;
+            while (k < n) {
+                k = k + x;
+            }
+            System.out.println(k);
+        }
     }
 
 }
