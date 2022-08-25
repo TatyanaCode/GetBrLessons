018d391513cda7c245a29b80f12bf0509c3d3ca2
danilova.tatyana <danilov.fedor@otr.ru>
2022-08-25 18:16:02 +0300

    day20082022

--- src/day18082022/Test24.java
+++ src/day18082022/Test24.java
@@ -10,21 +10,20 @@ public class Test24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();//сканируй количество слоев
-        int b = 1;//строка
-        String s = "*";
-        String v = " ";
+        int b = 1;//количество отнимаемых пробелов
+        String s = "*";// Вывод символа
+        String v = " ";// вывод пробела вместо *
 
-        while (b <= n) {
-            int c = 1;
-            while (c <= n - b)
-            {
-                System.out.print(" ");
-                c++;
+        while (b <= n) { //до тех пор пока количество слоев больше начального слоя
+            int c = 1;//считает сколько пробелов ставить, начальное знавение
+            while (c <= n - b) {//До тех пор пока количество пробелов меньше или равно
+                System.out.print(v);//  введение пробелов
+                c++;// + 1 пробел в каждом действии
             }
             System.out.println(s);
-            s += "**";
+            s += "**"; // к выведенному значению с каждым рядом + 2 **
             b++;
         }
-        }
     }
+}
 
