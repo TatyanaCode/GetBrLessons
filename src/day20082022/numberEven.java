6809c7fe8a69e0d60ce1530a406794e70b70be5f
danilova.tatyana <danilov.fedor@otr.ru>
2022-09-11 21:36:12 +0300

    day11092022

--- src/day20082022/numberEven.java
+++ src/day20082022/numberEven.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
  * Пользователь вводит с клавиатуры натуральное число n. Затем вводит n целых чисел.
  * Необходимо найти и вывести на экран количество чётных.
  */
-public class Test26 {
+public class numberEven {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         int n  = scanner.nextInt();// сканирование числа n
         int k = 0;      //Количество четных чисел
         for (int i = 0 ; i < n; i ++){//  до тех пор пока i<n
             int c = scanner.nextInt(); // сканируй c
-            if (c %2 ==0) { // если пр делении с остатком с =0 выведи с
+            if (c %2 ==0) { // если при делении с остатком с =0 выведи k
                 k++;
             }
         }
